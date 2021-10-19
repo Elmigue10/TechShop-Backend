ce2e34701699b36e6cc99ebee7c75f789f079d06
Daniel Gualteros <migued77@gmail.com>
2021-10-19 13:56:56 -0500

    Modulo del cliente

--- src/main/java/com/techshop/web/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.techshop.web.repository;
-
-import com.techshop.web.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    User findByUsername(String username);
-}
